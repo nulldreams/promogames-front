63ee26cedcf3f7926969a904b27f43bdd2b5f0e2
Igor Souza Martins <nulldreams@gmail.com>
2018-04-02 12:34:26 -0300

    feat: primeiro commit

---
+++ src/components/Principal.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <nuuvem/>
+  </div>
+</template>
+
+<script>
+  import nuuvem from '@/components/promocoes/nuuvem'
+  
+  export default {
+    name: 'principal',
+    components: {
+      nuuvem
+    }
+  }
+</script>
---
+++ src/components/promocoes/nuuvem.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="promocoes">
+    <div class="box" v-for="promo in promocoes" :key="promocoes">
+      <div class="card">
+        {{promo.nome}}
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+  import axios from 'axios'
+
+  export default {
+    name: "nuuvem",
+    data() {
+      return {
+        promocoes: [],
+        errors: []
+      }
+    },
+    created() {
+      axios
+        .get("https://promogames-nuuvem.herokuapp.com/promocoes/1")
+        .then(response => {
+          this.promocoes = response.data.promocoes
+        })
+        .catch(e => {
+          this.errors.push(e)
+        })
+    }
+  }
+</script>
